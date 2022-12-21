213dab775ff9f8e2cc070d83df19b85dbd96d238
Peculiar Erhisohwode <75625011+PeculiarE@users.noreply.github.com>
2022-12-21 03:26:04 +0100

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""This module contains a non-empty class."""
+
 class Square:
+    """A class defining a square by its size and calculating its area."""
     def __init__(self, size=0):
+         """A function initializing an instance of the class.
+        
+        Args:
+            size (int): The size of the square. Defaults to 0.
+
+        """
         self.__size = size
 
     # Property
@@ -19,4 +28,10 @@ class Square:
             self.__size = value
 
     def area(self):
+        """A function calculating the area of the square.
+
+        Returns:
+            int: The area of the square. 
+
+        """
         return self.__size ** 2
