e0af4d4464e5ce7c58cb653780cf4cb72ab115ab
Peculiar Erhisohwode <75625011+PeculiarE@users.noreply.github.com>
2022-12-21 01:55:04 +0100

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""This module contains a non-empty class."""
+
 class Square:
+    """A class defining a square by its size."""
     def __init__(self, size):
-        self.__size = size
+        """A function initializing an instance of the class.
+
+        Args:
+            size: The size of the square.
+
+        """
+        self.__size = size #: Private instance attribute
