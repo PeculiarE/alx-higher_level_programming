c155e13ed47cceebeb256a17987dce536bdb9369
Peculiar Erhisohwode <75625011+PeculiarE@users.noreply.github.com>
2022-12-21 02:31:45 +0100

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,13 +1,32 @@
 #!/usr/bin/python3
+"""This module contains a non-empty class."""
+
 class Square:
+    """A class defining a square by its size and calculating its area."""
     def __init__(self, size=0):
+        """A function initializing an instance of the class.
+        
+        Args:
+            size (int): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If `size` is not an integer.
+            ValueError: If `size` is less than 0.
+
+        """
         if type(size) != int:
             raise TypeError('size must be an integer')
         elif size < 0:
             raise ValueError('size must be >= 0')
         else:
-            self.__size = size
+            self.__size = size #: Private instance attribute
 
     def area(self):
+        """A function calculating the area of the square.
+
+        Returns:
+            int: The area of the square. 
+
+        """
         size = self.__size
         return size * size
